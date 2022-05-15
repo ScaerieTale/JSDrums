9437b95406555d4e1a4428ba1239c8194de3a4e9
ScaerieTale <7010800+ScaerieTale@users.noreply.github.com>
2022-05-14 22:28:29 -0500

    Added promised code comments and TODOs

--- index.js
+++ index.js
@@ -1,8 +1,22 @@
+/* Get the number of available
+   buttons using the .drum class
+   (in this case, the "length" 
+   of the list of .drum buttons) */
+
 var numberOfButtons = document.querySelectorAll(".drum").length;
 
+/* for each item in that list, add an
+   event listener to it that listens
+   for the button to be clicked on. */
 for (let i = 0; i < numberOfButtons; i++) {
     document.querySelectorAll(".drum")[i].addEventListener("click", function() {
         var buttonInnerHTML = this.innerHTML;
+        /* Don't ask me to explain
+        "this".  It works.  Thats all
+        I know.  Switch changes the
+        script to whatever button is
+        clicked and activates that
+        specific drum sound */
         switch (buttonInnerHTML) {
             case "w":
                 var tom1 = new Audio("sounds/tom-1.mp3");
@@ -35,3 +49,22 @@ for (let i = 0; i < numberOfButtons; i++) {
         }
     })
 }
+
+// TODO: Add keyboard eventlisteners!
+// Add Constructor(s)?  See below vvv
+/* Lessons learned so far: 
+    - I've learned how to use
+    QuerySelectorAll to manipulate
+    objects.
+    - I've learned what Objects are
+    (not to be confused with
+         'objects'
+         - I've learned what Constructors
+         are, even though we haven't 
+         used them yet so I expect
+         that soon.
+         - I've learned that "this" is
+         a thing that exists, that I
+         can tap into to manipulate
+         items.  For example:
+            this.style.color="white"*/
